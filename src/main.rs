e6eb2866702cee5725108348c67187f8f0148b30
-k <slowdive@me.com>
2022-12-15 17:47:34 -0800

    Add paging support

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,25 @@
 #![test_runner(hell::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use hell::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("HESHER WAS HERE!");
+entry_point!(kernel_main);
 
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use hell::memory::{self, BootInfoFrameAllocator};
+    use x86_64::{structures::paging::Page, VirtAddr};
+
+    println!("HESHER WAS HERE!");
     hell::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
     #[cfg(test)]
     test_main();
